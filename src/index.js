6b87666e068f882002e65c12f763d11f49b0601d
Joe Dou <joe.dou@gmail.com>
2020-07-23 23:07:21 -0700

    add babel

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(app.get("port"), function (error) {
+app.listen(app.get("port"), (error) => {
   if (error) {
+    // eslint-disable-next-line no-console
     console.error(error);
   } else {
+    // eslint-disable-next-line no-console
     console.info(`Listening on port ${app.get("port")}`);
   }
 });
